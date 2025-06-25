d4ac1c7ab41b68ac48dc430e5ba2fa75315d64cd
Mr Smile Modds <uceyheroku@gmail.com>
2025-06-25 21:02:16 +0300

    Add files via upload

---
+++ set.js
@@ -0,0 +1,33 @@
+global.prefix = '/'; // Set Your Preferred Prefix
+global.botName = 'TERMINATOR - X'; // Bot Name
+global.botVersion = '2.0.0';
+global.botPic = 'https://files.catbox.moe/mxxpd9.jpg'; // Can use path, can also use link
+global.botToken = '7935567739:AAH2fNdNVuX5nRJXwPL1zuFUz4vAX3QUvnM'; // Replace With Your Bot Token (Get it from BotFather)
+global.ownerUsername = 'Darktech65_bot'; // Replace With Your Telegram Username(For Bot Owner)
+global.timeZone = 'Africa/Nairobi'; // Your Time Zone
+global.ownerId = [5153324742]; // Replace With Your Owner ID(For Bot Owner), You Can Add More than 1
+global.footer = '*Powered by Mr Smile*';
+global.ytdlWeb = 'https://youtube.com/@mrsmile_modders?si=L2gkUMaKTCPO2IOw';
+global.ytdlApi = 'https://giftedtech-ytdl-api.hf.space';
+global.giftedApi = 'https://apis.giftedtech.web.id/api'; // Gifted Api
+global.giftedKey = 'gifted'; // Gifted Api Key
+global.maherApi = 'https://api.nexoracle.com'; // NexOracle Api
+global.maherKey = ''; // NexOracle Api Key
+global.lyffeApi = 'https://levanter.onrender.com'; // Lyffe Api
+global.giftedWaChannel = 'https://whatsapp.com/channel/0029VaesBAXJJhzefVszDu3h';
+global.giftedRepo = "https://github.com/Jaydendev112/telegram-bot";
+global.giftedApiRepo = "https://api.github.com/repos/Jaydendev112/telegram-bot";
+
+global.giftechMess = {
+    internalerr: 'Internal Server Error!',
+    owner: 'Owner-Only Feature!',
+    group: 'Use in a Group!',
+    private: 'Use in a Private Chat!',
+    wait: 'Please Wait...',
+    done: 'Completed Successfully!',
+    error: 'Oops, an Error Occurred'
+};
+
+String.prototype.capitalize = function () {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+};
